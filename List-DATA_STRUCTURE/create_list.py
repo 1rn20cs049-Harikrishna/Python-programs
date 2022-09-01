ab97cc8a922c586ab9ef68a119aab6dc37a69c62
Harikrishna R <95685588+1rn20cs049-Harikrishna@users.noreply.github.com>
2022-09-02 00:25:42 +0530

    Creating list and print
    Creating the list of size user input and appending the list by using the append method

---
+++ List-DATA_STRUCTURE/create_list.py
@@ -0,0 +1,8 @@
+num = int(input()) #size of the list
+list_a = [] #empty list
+
+for i in range(0,num):
+    context = input()  #item for the list to be added
+    list_a.append(context) #appending the lis
+    
+print(list_a)
